29b3e81c4954e5b9a4811e59d74a4190853a1abb
RenanMarinho809 <renanmarinho809@gmail.com>
2025-03-04 12:35:31 -0300

    feat: update drivers and teams data with additional entries and corrections

--- src/F1drivers/driver.ts
+++ src/F1drivers/driver.ts
@@ -1,5 +1,102 @@
 export const drivers = [
-    { id: 1, name: "Max Verstappen", team: "Red Bull Racing" },
-    { id: 2, name: "Lewis Hamilton", team: "Ferrari" },
-    { id: 2, name: "Lando Norris", team: "McLaren" },
-  ];
+  {
+    id: 1,
+    name: "Max Verstappen",
+    team: "Red Bull Racing",
+  },
+  {
+    id: 44,
+    name: "Lewis Hamilton",
+    team: "Ferrari",
+  },
+  {
+    id: 16,
+    name: "Charles Leclerc",
+    team: "Ferrari",
+  },
+  {
+    id: 4,
+    name: "Lando Norris",
+    team: "McLaren",
+  },
+  {
+    id: 63,
+    name: "George Russell",
+    team: "Mercedes",
+  },
+  {
+    id: 14,
+    name: "Fernando Alonso",
+    team: "Aston Martin",
+  },
+  {
+    id: 18,
+    name: "Lance Stroll",
+    team: "Aston Martin",
+  },
+  {
+    id: 30,
+    name: "Liam Lawson",
+    team: "Red Bull Racing",
+  },
+  {
+    id: 81,
+    name: "Oscar Piastri",
+    team: "McLaren",
+  },
+  {
+    id: 87,
+    name: "Oliver Bearman",
+    team: "Haas",
+  },
+  {
+    id: 7,
+    name: "Jack Doohan",
+    team: "Alpine",
+  },
+  {
+    id: 10,
+    name: "Pierre Gasly",
+    team: "Alpine",
+  },
+  {
+    id: 23,
+    name: "Alexander Albon",
+    team: "Williams",
+  },
+  {
+    id: 27,
+    name: "Nico Hulkenberg",
+    team: "Sauber",
+  },
+  {
+    id: 31,
+    name: "Esteban Ocon",
+    team: "Haas",
+  },
+  {
+    id: 5,
+    name: "Gabriel Bortoleto",
+    team: "Sauber",
+  },
+  {
+    id: 55,
+    name: "Carlos Sainz",
+    team: "Williams",
+  },
+  {
+    id: 12,
+    name: "Andrea Kimi Antonelli",
+    team: "Mercedes",
+  },
+  {
+    id: 22,
+    name: "Yuki Tsunoda",
+    team: "Racing Bulls",
+  },
+  {
+    id: 6,
+    name: "Isack Hadjar",
+    team: "Racing Bulls",
+  }
+];
